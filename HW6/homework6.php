cb05781b0bf915a9a56ed4470d56b6484907d88f
vikashanumegowda <hanumego@usc.edu>
2017-10-05 23:46:31 -0700

    Initial commit without form

--- HW6/homework6.php
+++ HW6/homework6.php
@@ -1,9 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: vikashanumegowda
- * Date: 10/5/17
- * Time: 10:12 PM
- */
+<html>
+    <head>
+        <title>
+            Homework 6
+        </title>
+    </head>
+    <body>
+        <?php if(isset($_POST["Search"])): ?>
+        <?php else: ?>
+            <div style="width: 500px; height: auto; background: #C0C0C0;  margin-left: auto; margin-right: auto; padding-left: 2px; padding-right: 2px;">
+                <div class="center" style="font-size: 40px;text-align: center;">Stock Search</div><hr>
+                <div style="font-size: 20px;">Enter Stock Ticker Symbol:*</div>
+            </div>
+        <?php endif; ?>
 
-?>
+
+    </body>
+</html>
