6b98101ab25ad91c32bad4f671b53f4703108bdc
vikashanumegowda <hanumego@usc.edu>
2017-10-06 23:00:31 -0700

    Static page withs styling

--- HW6/homework6.php
+++ HW6/homework6.php
@@ -6,10 +6,19 @@
     </head>
     <body>
         <?php if(isset($_POST["Search"])): ?>
+            <p>Hello World!!</p>
         <?php else: ?>
-            <div style="width: 500px; height: auto; background: #C0C0C0;  margin-left: auto; margin-right: auto; padding-left: 2px; padding-right: 2px;">
-                <div class="center" style="font-size: 40px;text-align: center;">Stock Search</div><hr>
-                <div style="font-size: 20px;">Enter Stock Ticker Symbol:*</div>
+            <div style="width: 500px; height: auto; background: #C0C0C0;  margin-left: auto; margin-right: auto; padding-left: 2px; padding-right: 2px; border-style: solid; border-width: 1px; border-color: #808080;">
+                <div  style="font-size: 40px;text-align: center;">Stock Search</div><hr style="color: #808080;">
+                <form>
+                    <div >
+                        <p style="font-size: 20px;display: inline-block;">Enter Stock Ticker Symbol:*</p>
+                        <input style="display: inline-block; width: 50%;" type="text" title="STS"><br>
+                        <input style="font-size: 20px; margin-left: 245px;margin-right: 10px;" type="submit" value="Search">
+                        <input style="font-size: 20px;" type="button" value="Clear">
+                    </div>
+                </form>
+                <p>* - <i>Mandatory fields.</i></p>
             </div>
         <?php endif; ?>
 
