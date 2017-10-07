bb9c83ba5b3d04066863db2df07a4225a9bcf86e
vikashanumegowda <hanumego@usc.edu>
2017-10-07 16:31:33 -0700

    Error case of requesting quotes for unknown symbol done.

--- HW6/ready_up.php
+++ HW6/ready_up.php
@@ -3,9 +3,17 @@
         <title>
             Homework 6
         </title>
+        <style>
+            body {
+                min-width:650px;
+            }
+            td {
+                padding: 0px;
+            }
+        </style>
     </head>
 <body>
-            <div style="width: 500px; height: auto; background: #C0C0C0;  margin-left: auto; margin-right: auto; padding-left: 2px; padding-right: 2px; border-style: solid; border-width: 1px; border-color: #808080;">
+            <div style="width: 500px; height: auto; background: #C0C0C0;  margin-left: auto; margin-right: auto; padding-left: 2px; padding-right: 2px; border-style: solid; border-width: 1px; border-color: #808080; margin-bottom: 15px;">
                 <div style="font-size: 40px;text-align: center;">Stock Search</div>
                 <hr style="color: #808080;">
                 <form method=POST action=<?php echo $_SERVER["PHP_SELF"]; ?>>
@@ -18,7 +26,13 @@
                 </form>
                 <p>* - <i>Mandatory fields.</i></p>
             </div>
-            <?php function test_input($data)
+            <?php
+            function startsWith($haystack, $needle)
+            {
+                $length = strlen($needle);
+                return (substr($haystack, 0, $length) === $needle);
+            }
+            function test_input($data)
             {
                 $data = trim($data);
                 $data = stripslashes($data);
@@ -33,9 +47,22 @@
                     $symbol = test_input($_POST["STS"]);
                     $url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" . $symbol . "&apikey=OGY0S9LG8J8ADNZW";
                     $response = file_get_contents($url);
-                    echo $response;
-                    $response = json_decode($response);
-
+//                    echo $response;
+                    $response = json_decode($response,true);
+                    //echo $response->"Error Message";
+                    if (startsWith($response["Error Message"],"Invalid")):
+                    {
+                          echo "<table style=\"text-align:center; margin:0px; border-width:1px; padding:0px; border-style:solid; border-color:#888; width:100%\">";
+                          echo "<tr><th style=\"height:100%; background:#D0D0D0; border-right: solid; border-color: #808080; border-width: 1px; padding: 0px;\">Error</th><td>Error: NO record has been found, please enter a valid symbol</td></tr>";
+                          echo "</table>";
+                    }
+                    else:
+                    {
+                        echo "<table style=\"text-align:center; margin:0px; border-width:1px; padding:0px; border-style:solid; border-color:#888; width:100%\">";
+                        echo "<tr><th style=\"height:100%; background:#D0D0D0; max-width: 300px; border-right: solid; border-color: #808080; border-width: 1px; padding: 0px;\"></th><td>Error: NO record has been found, please enter a valid symbol</td></tr>";
+                        echo "</table>";
+                    }
+                    endif;
                 }
                 endif;
             }
