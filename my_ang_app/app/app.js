9ed59c5d6f5af13b221ae3bfb96d2b5284c06c23
vikashanumegowda <hanumego@usc.edu>
2017-11-11 13:47:37 -0800

    the transition to details section done ng-show ng-hide

--- my_ang_app/app/app.js
+++ my_ang_app/app/app.js
@@ -1,12 +1,10 @@
+var app = angular.module('stockApp', ['ngAnimate', 'ngMaterial']);
 
+app.controller('myCtrl', function ($scope, $http) {
 
-var app = angular.module('stockApp', ['ngAnimate','ngMaterial']);
-
-app.controller('myCtrl', function($scope, $http){
-
-    this.querySearch = function(query){
-        return $http.get("http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/?symbol="+query+"&second=mark")
-            .then(function(response){
+    this.querySearch = function (query) {
+        return $http.get("http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/?symbol=" + query + "&second=mark")
+            .then(function (response) {
                 console.log("hello");
                 return JSON.parse(response.data);
             })
@@ -22,8 +20,7 @@ app.controller('myCtrl', function($scope, $http){
             .replace(/'/g, "&#039;");
     }
 
-    function test_input(symbol)
-    {
+    function test_input(symbol) {
         symbol = symbol.trim();
         console.log(symbol);
         symbol = escapeHtml(symbol);
@@ -31,16 +28,21 @@ app.controller('myCtrl', function($scope, $http){
     }
 
     $scope.symbol_typed = "";
-    $scope.symbol="";
+    $scope.symbol = "";
 
     $scope.submit = function () {
+        $scope.hide_fav = true;
+        $scope.show_fav = false;
+
+        $scope.hide_details = false;
+        $scope.show_details = true;
         symbol = $("#inputSymbol").val();//value from the input text field
         $scope.symbol = symbol;
         // console.log("valueis Ola!");
-        console.log("valueis"+symbol);
-        console.log("valueis"+$scope.symbol);
+        console.log("valueis" + symbol);
+        console.log("valueis" + $scope.symbol);
         $scope.symbol = test_input(symbol);
-        if(!symbol) {
+        if (!symbol) {
             $('#inputSymbol').tooltip('enable');
             $('#inputSymbol').tooltip('show');
             return;
@@ -49,13 +51,13 @@ app.controller('myCtrl', function($scope, $http){
             $('#inputSymbol').tooltip('disable');
         }
         // $scope.showTableWait();
-        $scope.activeProgressbar=true;
+        $scope.activeProgressbar = true;
         // var lat= $scope.lat;
         // var long=$scope.long;
         $http({
-            method:'GET',
-            url:"http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
-            params:{"symbol": $scope.symbol, "second": "news"}
+            method: 'GET',
+            url: "http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
+            params: {"symbol": $scope.symbol, "second": "news"}
         }).then(function successCallback(response) {
             $scope.news = response.data.channel.item;
             console.log($scope.news[0]);
@@ -66,103 +68,78 @@ app.controller('myCtrl', function($scope, $http){
             // });
 
 
-            $http({
-                method:'GET',
-                url:"http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
-                params:{"symbol": $scope.symbol, "second": "tsd"}
-            }).then(function successCallback(response) {
-                $scope.tsd = response.data;
-                console.log("tsd");
-                console.log($scope.tsd);
-                // $scope.pageData = response.data.data;
-                // console.log(response.data.data);
-                // $scope.pagePage=response.data.paging;
-                // if( !$scope.pagePage)
-                // {
-                //     $scope.pageAppend['hasNext']=false;
-                //     $scope.pageAppend['hasPrev']=false;
-                // }
-                // else {
-                //     $scope.pageAppend=$scope.append($scope.pagePage,"page");
-                //     console.log(response.data.paging);
-                // }
-                // angular.forEach($scope.pageData, function(entry) {
-                //     entry.isFav = $scope.isStarred(entry.id);
-                // });
+        });
+        $http({
+            method: 'GET',
+            url: "http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
+            params: {"symbol": $scope.symbol, "second": "tsd"}
+        }).then(function successCallback(response) {
+            $scope.tsd = response.data;
+            console.log("tsd");
+            console.log($scope.tsd);
+
+            // angular.forEach($scope.pageData, function(entry) {
+            //     entry.isFav = $scope.isStarred(entry.id);
+            // });
 
 
-            //     $http({
-            //         method:'GET',
-            //         url:"http://www.sodium-task-95910.appspot.com/?",
-            //         params:{"data": keyword, "type": "event"}
-            //     }).then(function successCallback(response) {
-            //         $scope.eventData = response.data.data;
-            //         console.log(  response.data.paging );
-            //         $scope.eventPage=response.data.paging;
-            //         if( !$scope.eventPage)
-            //         {
-            //             $scope.eventAppend['hasNext']=false;
-            //             $scope.eventAppend['hasPrev']=false;
-            //         }
-            //         else {
-            //             $scope.eventAppend=$scope.append($scope.eventPage,"event");
-            //             console.log(response.data.paging);
-            //         }
+        });
+        $http({
+            method: 'GET',
+            url: "http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
+            params: {"symbol": $scope.symbol, "second": "stoch"}
+        }).then(function successCallback(response) {
+            $scope.stoch = response.data;
+            console.log("stoch");
+            console.log($scope.stoch);
+
             //         angular.forEach($scope.eventData, function(entry) {
             //             entry.isFav = $scope.isStarred(entry.id);
             //         });
             //         //    console.log( response.data );
-            //         $http({
-            //             method:'GET',
-            //             url:"http://www.sodium-task-95910.appspot.com/?",
-            //             params:{"data": keyword, "type": "place","lat": lat,"long":long}
-            //         }).then(function successCallback(response) {
-            //             $scope.placeData = response.data.data;
-            //             console.log( response.data.paging );
-            //             $scope.placePage=response.data.paging;
-            //             if( !$scope.placePage)
-            //             {
-            //                 $scope.placeAppend['hasNext']=false;
-            //                 $scope.placeAppend['hasPrev']=false;
-            //             }
-            //             else {
-            //                 $scope.placeAppend=$scope.append($scope.placePage,"place");
-            //                 console.log(response.data.paging);
-            //             }
+        });
+        $http({
+            method: 'GET',
+            url: "http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
+            params: {"symbol": $scope.symbol, "second": "bbands"}
+        }).then(function successCallback(response) {
+            $scope.bbands = response.data;
+            console.log("bbands");
+            console.log($scope.bbands);
             //             angular.forEach($scope.placeData, function(entry) {
             //                 entry.isFav = $scope.isStarred(entry.id);
             //             });
             //             //   console.log( response.data );
-            //             $http({
-            //                 method:'GET',
-            //                 url:"http://www.sodium-task-95910.appspot.com/?",
-            //                 params:{"data": keyword, "type": "group"}
-            //             }).then(function successCallback(response) {
-            //                 $scope.groupData = response.data.data;
-            //                 console.log(response.data.paging);
-            //                 $scope.groupPage=response.data.paging;
-            //                 if( !$scope.groupPage)
-            //                 {
-            //                     $scope.groupAppend['hasNext']=false;
-            //                     $scope.groupAppend['hasPrev']=false;
-            //                 }
-            //                 else {
-            //                     $scope.groupAppend=$scope.append($scope.groupPage,"group");
-            //                     console.log(response.data.paging);
-            //                 }
+        });
+        $http({
+            method: 'GET',
+            url: "http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
+            params: {"symbol": $scope.symbol, "second": "sma"}
+        }).then(function successCallback(response) {
+            $scope.sma = response.data;
+            console.log("sma");
+            console.log($scope.sma);
             //                 angular.forEach($scope.groupData, function(entry) {
             //                     entry.isFav = $scope.isStarred(entry.id);
             //                 });
-            //                 /*Rendering Starts*/
-            //                 $scope.setData($scope.id);
-            //             });
-            //         });
-            //     });
+            $http({
+                method: 'GET',
+                url: "http://homework8-env.wjdp2sdqus.us-west-2.elasticbeanstalk.com/",
+                params: {"symbol": $scope.symbol, "second": "ema"}
+            }).then(function successCallback(response) {
+                $scope.ema = response.data;
+                console.log("ema");
+                console.log($scope.ema);
+                //                 /*Rendering Starts*/
+                //                 $scope.setData($scope.id);
             });
         });
+
+
     }
 
     $('#inputSymbol').tooltip('disable');
     $('#inputSymbol').tooltip('hide');
     $scope.symbol_typed = "";
-});
+})
+;
